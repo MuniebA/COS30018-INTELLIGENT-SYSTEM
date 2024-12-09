a54079eba06c0abb35b2577d64ac1460c4351edd
MuniebA <muniebawad@gmail.com>
2024-12-09 14:34:44 +0800

    Add large files using Git LFS

--- Gui/Total_frames.py
+++ Gui/Total_frames.py
@@ -1,35 +1,3 @@
-import cv2
-
-
-def get_video_info(video_path):
-    cap = cv2.VideoCapture(video_path)
-    if not cap.isOpened():
-        print("Error: Could not open video.")
-        return None
-
-    # Get the frame rate
-    fps = cap.get(cv2.CAP_PROP_FPS)
-
-    # Get the total frame count
-    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-
-    # Calculate the duration in seconds
-    duration = frame_count / fps
-
-    cap.release()
-
-    return {
-        "fps": fps,
-        "frame_count": frame_count,
-        "duration": duration
-    }
-
-
-# Replace with the path to your video file
-video_path = 'static/videos/test_2.mp4'
-video_info = get_video_info(video_path)
-
-if video_info:
-    print(f"Frame rate: {video_info['fps']} fps")
-    print(f"Total frames: {video_info['frame_count']}")
-    print(f"Duration: {video_info['duration']} seconds")
+version https://git-lfs.github.com/spec/v1
+oid sha256:b0e3ec9899eec569b2e39df9967d7c5a67bf0800502918e44097edbe4d4b3bae
+size 866
